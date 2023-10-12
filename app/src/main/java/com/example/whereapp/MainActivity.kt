3f857222ecaf4cac2c21516bed9d53132a836645
wwIrvingww <mor22781@guvg.edu.gt>
2023-10-12 09:26:08 -0600

    se modifico la estructura y se mejora la vista del Login

--- app/src/main/java/com/example/whereapp/MainActivity.kt
+++ app/src/main/java/com/example/whereapp/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.whereapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
-
+import com.example.whereapp.ui.theme.login.view.LoginScreen
 
 
 //import com.example.whereapp.navigation.Navigation
@@ -14,7 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //Navigation()
+            LoginScreen()
         }
     }
 }
