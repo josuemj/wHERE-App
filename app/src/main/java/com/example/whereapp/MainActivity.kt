800760effd3781257348964d7adaf34944889c54
wwIrvingww <mor22781@guvg.edu.gt>
2023-10-12 00:04:27 -0600

    se modificó el Login para que tenga una apariencia más atractiva

--- app/src/main/java/com/example/whereapp/MainActivity.kt
+++ app/src/main/java/com/example/whereapp/MainActivity.kt
@@ -3,19 +3,22 @@ package com.example.whereapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-/**
- *
 
-import com.example.whereapp.navigation.Navigation
+
+
+
+//import com.example.whereapp.navigation.Navigation
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
+            //Navigation()
         }
     }
 }
- */
+
+
+
 
